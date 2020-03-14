678b0811b0da7dda98854c232476ba52842846f5
Arpitkumar Patel <47935750+arpitpatel1501@users.noreply.github.com>
2020-03-15 03:35:06 +0530

    Add files via upload
    Convert Text to Audio file

---
+++ textTOaudio.py
@@ -0,0 +1,52 @@
+
+!pip install pyttsx3==2.7
+!pip install talkey
+!pip install py-espeak-ng
+!pip install gTTS
+
+'''
+import pyttsx3
+engine = pyttsx3.init() # object creation
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 125)     # setting up new voice rate
+
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+print (volume)                          #printing current volume level
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+engine.say("Hello World!")
+engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+'''
+
+from gtts import gTTS 
+
+mytext = full_text
+
+# Language in which you want to convert 
+language = 'en'
+
+# Passing the text and language to the engine,  
+# here we have marked slow=False. Which tells  
+# the module that the converted audio should  
+# have a high speed 
+myobj = gTTS(text=mytext, lang=language, slow=True)
+
+myobj.save("/content/drive/My Drive/Hackathon_2020_Enigma/Video-Text/welcome.wav")
+
+
+
+from google.colab import drive
+drive.mount('/content/drive')
+
